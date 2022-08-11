0f58af59ca06e0d9555a68cf428b9191b9b59142
asa-naki <aki.grade2580@outlook.jp>
2022-08-11 14:30:31 +0900

    deleate param

--- include/robotx_costmap_calculator/costmap_calculator_component.hpp
+++ include/robotx_costmap_calculator/costmap_calculator_component.hpp
@@ -101,7 +101,6 @@ private:
   void pointCloudCallback(const sensor_msgs::msg::PointCloud2::SharedPtr cloud);
   void scanCallback(const sensor_msgs::msg::LaserScan::SharedPtr scan);
   void poseCallback(const geometry_msgs::msg::PoseStamped::SharedPtr pose);
-  boost::circular_buffer<grid_map::GridMap> map_data_;
   boost::circular_buffer<sensor_msgs::msg::PointCloud2> cloud_buffer_;
   boost::circular_buffer<sensor_msgs::msg::LaserScan> scan_buffer_;
   grid_map::GridMap combine_map;
@@ -114,7 +113,6 @@ private:
   grid_map::Matrix getPointCloudToGridMap(
     const sensor_msgs::msg::PointCloud2 & cloud, const std::string & grid_map_layer_name);
   std::string points_raw_topic_;
-  grid_map::Matrix grid_map_data_;
   std::string laserscan_raw_topic_;
   std::string output_topic_;
   std::string current_pose_topic;
